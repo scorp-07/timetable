b760dd2ef613537004920e4a89fbcff655c0a9bc
scorp-07 <web.design.shah@gmail.com>
2020-08-25 14:06:14 +0300

    vervison 0.2

--- public/js/main.js
+++ public/js/main.js
@@ -2,43 +2,48 @@ const addBtn = document.getElementsByClassName('add-btn');
 const outBlock = document.getElementById('out');
 const courseEl = document.getElementsByClassName('course__btn');
 const filterEl = document.getElementById('filter');
-const specEl = document.getElementsByClassName('speciality__btn');
+const editDate = [];
 
 const specialities = ['Web-design', 'Дизайни робитавӣ', 'Дизайни кастюм', 'Дизайни графикӣ'];
+const coursess = ['Курси 1'];
 const editGroup = {};
 let idCount = 4;
+
+for (const item of courseEl) {
+    item.addEventListener('click', () => {
+        const course = item.dataset.course;
+        editDate[0] = course;
+        filterEl.innerHTML = '';
+        for (const el of specialities) {
+            filterEl.innerHTML += `<div class="filter__item speciality__btn" data-speciality="2">${el}</div>`;
+        }
+        const specEl = document.getElementsByClassName('speciality__btn');
+        for (const item of specEl) {
+            item.addEventListener('click', () => {
+                filterEl.style.display = 'none';
+                document.getElementById('table').style.display = 'flex';
+            });
+        }
+    });
+}
+
+
 
 for (const item of addBtn) {
     item.addEventListener('click', () => {
         const pBlock = item.parentElement;
         idCount++;
-        console.log(pBlock.classList);
         item.before(createInputs());
     });
 }
 
-function createInputs(outBlock) {
-    const inputs = document.createElement('div');
-    inputs.classList.add('inputs');
-    const timeStartInput = document.createElement('input');
-    timeStartInput.type = 'time';
-    timeStartInput.classList = 'lesson-input';
-    timeStartInput.id = "lesson-start";
-    timeStartInput.value = '08:00';
-    const timeEndInput = document.createElement('input');
-    timeEndInput.type = 'time';
-    timeEndInput.classList = 'lesson-input';
-    timeEndInput.id = "lesson-end";
-    timeEndInput.value = "08:00";
-    const lessonName = document.createElement('select');
-    lessonName.classList = 'lesson-input lesson-select';
-    lessonName.id = "lesson";
-    const classRoom = document.createElement('select');
-    classRoom.classList = 'lesson-input lesson-select';
-    classRoom.id = "classroom";
-    const teacherName = document.createElement('select');
-    teacherName.classList = 'lesson-input lesson-select';
-    teacherName.id = "teacher";
+function createInputs() {
+    const inputs = createEl('div', { 'class': 'inputs' });
+    const timeStartInput = createEl('input', { 'type': 'time', 'class': 'lesson-input', 'id': 'leson-start', 'value': '08:00' });
+    const timeEndInput = createEl('input', { 'type': 'time', 'class': 'lesson-input', 'id': 'lesson-end', 'value': '08:00' });
+    const lessonName = createEl('select', { 'class': 'lesson-input lesson-select', 'id': 'lesson' });
+    const classRoom = createEl('select', { 'class': 'lesson-input lesson-select', 'id': 'classroom' });
+    const teacherName = createEl('select', { 'class': 'lesson-input lesson-select', 'id': 'teacher' });
 
     inputs.appendChild(timeStartInput);
     inputs.appendChild(timeEndInput);
@@ -48,8 +53,8 @@ function createInputs(outBlock) {
     return inputs;
 }
 
-/*
 
+/*
 function _ajax(url, method, obj)
 {
 
@@ -72,9 +77,20 @@ request.responseText;
 
 request.send(sUrl(obj));
 }
-
-
-
 */
 
+
+function addEvent(elements, e, callback) {
+    for (const item of elements) {
+        item.addEventListener(e, callback);
+    }
+}
+
+function createEl(name, options) {
+    const elem = document.createElement(name);
+    for (const key in options) {
+        elem.setAttribute(key, options[key]);
+    }
+    return elem;
+}
 
